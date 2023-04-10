995e858d46509a286621790b2623bb266320c329
xinLan <1352776732@qq.com>
2023-04-11 00:06:51 +0800

    更新

--- xxl-multipledb-mybatisplus/src/java/com/xxl/multipledb/dao/SysUserDao.java
+++ xxl-multipledb-mybatisplus/src/java/com/xxl/multipledb/dao/SysUserDao.java
@@ -2,9 +2,10 @@ package com.xxl.multipledb.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xxl.multipledb.entity.SysUser;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-
+@Mapper
 public interface SysUserDao extends BaseMapper<SysUser> {
     List<SysUser> sysUserList();
 }
--- xxl-mydemo/src/main/java/com/xxl/mydemo/demos/helloworld/controller/HelloWorldController.java
+++ xxl-mydemo/src/main/java/com/xxl/mydemo/demos/helloworld/controller/HelloWorldController.java
@@ -1,16 +1,35 @@
 package com.xxl.mydemo.demos.helloworld.controller;
 
 import com.xxl.mydemo.demos.helloworld.entity.User;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+import java.util.Map;
 
 @RestController
 @RequestMapping("hello")
 public class HelloWorldController {
+
+    @Value("${spring.datasource.default.url}")
+    private String myUrl;
+
     @RequestMapping("user")
     @ResponseBody
     public User myUser(@RequestBody User user, @RequestParam String sex){
         user.setSex(sex);
         return user;
     }
+
+    @RequestMapping("yml")
+    @ResponseBody
+    public Map getYmlPro(){
+        Class<?> clazz = HelloWorldController.class;
+        InputStream inputStream = clazz.getResourceAsStream("/application.yml");
+        Yaml yaml = new Yaml();
+        Map<String, Object> yamlData = yaml.load(inputStream);
+        return yamlData;
+    }
+
 }
